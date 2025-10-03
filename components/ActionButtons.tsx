de8064101503010a2d26067a80288a6f527a8f2e
Forhad Reza <forhad.bimt@gmail.com>
2025-10-04 00:04:25 +0600

    chore: added some necessary packages

---
+++ components/ActionButtons.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+
+
+ const ActionButtons = ({}) => {
+  return (
+    <div>
+      ActionButtons component
+    </div>
+  );
+};
+
+export default ActionButtons;
+
+
+
+// import React from 'react'
+
+// export default function ActionButtons() {
+//   return (
+//     <div>ActionButtons</div>
+//   )
+// }
+
+
+import * as React from 'react';
+
+export interface IAppProps {
+}
+
+export function App (props: IAppProps) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import React from "react";
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="container flex justify-between items-center py-4">
+        <div className="nav-brand">
+          <a href="index.html">
+            <Image src="logo.png" alt="Eventry" width={135} height={135} />
+          </a>
+        </div>
+        <ul className="flex gap-4 text-[#9C9C9C]">
+          <li>About</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
