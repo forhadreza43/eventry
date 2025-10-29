cc5a6ca4c947332cbd4914b79559162b0611512f
Forhad Reza <forhad.bimt@gmail.com>
2025-10-29 19:17:16 +0600

    Add ActionButtons component type safety and visual tweaks

--- components/ActionButtons.tsx
+++ components/ActionButtons.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
 import React from "react";
 
-const ActionButtons = ({ fromDetails }: { fromDetails: string }) => {
+const ActionButtons = ({ fromDetails }: { fromDetails: boolean }) => {
   return (
     <div className={`w-full flex gap-4 mt-4 ${fromDetails && "flex-1"}`}>
-      <button className="w-full bg-indigo-600 hover:bg-indigo-800">
+      <button className="w-full bg-indigo-600 hover:bg-indigo-800 border-indigo-700">
         Interested
       </button>
       <Link
