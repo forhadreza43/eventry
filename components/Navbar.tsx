31675930d9e45a8cafa2b23badd219b3e92a2c47
Forhad Reza <forhad.bimt@gmail.com>
2025-10-29 23:23:22 +0600

    fix: Update Navbar logo import to use image import instead of static path

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import logo from "../public/logo.png";
 export default function Navbar() {
   return (
     <nav>
       <div className="container mx-auto flex justify-between items-center py-4">
         <div className="nav-brand">
           <Link href="/" >
-            <Image src="/logo.png" alt="Eventry" width={135} height={135} />
+            <Image src={logo} alt="Eventry" width={135} height={135} />
           </Link>
         </div>
         <ul className="flex gap-4 text-[#9C9C9C]">
