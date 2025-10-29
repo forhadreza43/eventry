a4642c86dddda1153f3ffe242b134c4821edae7a
Forhad Reza <forhad.bimt@gmail.com>
2025-10-29 19:17:16 +0600

    Implement navigation bar with routing and styling improvements

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,22 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 export default function Navbar() {
   return (
     <nav>
-      <div className="container flex justify-between items-center py-4">
+      <div className="container mx-auto flex justify-between items-center py-4">
         <div className="nav-brand">
-          <a href="index.html">
-            <Image src="logo.png" alt="Eventry" width={135} height={135} />
-          </a>
+          <Link href="/" >
+            <Image src="/logo.png" alt="Eventry" width={135} height={135} />
+          </Link>
         </div>
         <ul className="flex gap-4 text-[#9C9C9C]">
-          <li>About</li>
-          <li>Contact Us</li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact Us</Link>
+          </li>
         </ul>
       </div>
     </nav>
