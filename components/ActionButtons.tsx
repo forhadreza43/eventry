4505947b1160395ebb6024c11bfc6a06b4220bc3
Forhad Reza <forhad.bimt@gmail.com>
2025-11-06 00:59:50 +0600

    Refactor ActionButtons state usage and button logic

--- components/ActionButtons.tsx
+++ components/ActionButtons.tsx
@@ -22,15 +22,9 @@ const ActionButtons = ({
   const isInterested = user && interested_ids.includes(user.id!.toString());
   const isGoing = user && going_ids.includes(user.id!.toString());
 
-  const [going] = useState(isGoing);
-  const [interested] = useState(isInterested);
-
   async function toggleInterest() {
     if (user) {
-      // console.log("User:", user);
-      // console.log(user);
       await updateEventInterest(user.id!, eventId);
-      router.refresh();
     } else {
       router.push("/login");
     }
@@ -48,7 +42,7 @@ const ActionButtons = ({
     <div className={`w-full flex gap-4 mt-4 ${fromDetails && "flex-1"}`}>
       <button
         className={`w-full hover:bg-indigo-800  ${
-          interested
+          isInterested
             ? "bg-indigo-800 border-indigo-700"
             : "bg-[#464849] border-[#5F5F5F]/50"
         } `}
@@ -59,9 +53,9 @@ const ActionButtons = ({
       <button
         onClick={handleGoing}
         className={` text-center w-full bg-[#464849] py-2 px-2 rounded-md border border-[#5F5F5F]/50 shadow-sm hover:bg-[#3C3D3D] transition-colors active:translate-y-1 ${
-          going ? "opacity-50 cursor-not-allowed" : "cursor-pointer"
+          isGoing ? "opacity-50 cursor-not-allowed" : "cursor-pointer"
         }`}
-        disabled={user! && going!}
+        disabled={user! && isGoing!}
       >
         Going
       </button>
